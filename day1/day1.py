0aab5a212a415452ad161a8f02ba75b63cf257a2
TSalten <saltenis.tadas@gmail.com>
2021-12-01 16:38:11 +0200

    day1 removed sum from p2 and general optimizations

--- day1/day1.py
+++ day1/day1.py
@@ -7,25 +7,19 @@ def xd():
     # data = list(map(int, data))
 
     # p1 = 0
-    # prev = data[0]
-    # for line in data:
-    #     curr = line
-    #     if curr > prev:
+    # for i in range(len(data)):
+    #     if data[i] > data[i-1]:
     #         p1 += 1
-    #     prev = curr
 
     # p2 = 0
-    # prev = sum(data[:3])
-    # for i in range(1, len(data)-2):
-    #     curr = sum(data[i:i+3])
-    #     if curr > prev:
+    # for i in range(3, len(data)):
+    #     if data[i] > data[i-3]:
     #         p2 += 1
-    #     prev = curr
 
     # print(f"{p1=}")
     # print(f"{p2=}")
 
-    print("p1="+str(sum(map(lambda i: data[i] > data[i-1], range(1, len(data := list(map(int, open(str(Path(__file__).parent.resolve()) + "/input")))))))), "\np2=" + str(sum(map(lambda i: sum(data[i:i+3]) > sum(data[i-1:i+2]), range(1, len(data))))))
+    print("p1="+str(sum(map(lambda i: data[i] > data[i-1], range(1, len(data := list(map(int, open(str(Path(__file__).parent.resolve()) + "/input")))))))), "\np2=" + str(sum(map(lambda i: data[i] > data[i-3], range(3, len(data))))))
 
 
 if __name__ == "__main__":
