540b670bcfceab71b111de301985318712c4154c
TSalten <saltenis.tadas@gmail.com>
2021-12-03 10:51:44 +0200

    day3 clean

--- day3/day3.py
+++ day3/day3.py
@@ -2,48 +2,31 @@ from pathlib import Path
 from functools import reduce
 
 
-def xd():
-    with open(str(Path(__file__).parent.resolve()) + "/input") as f:
-        data = f.read()
+def fltr(data, compare):
+    i = 0
+    filtered = list(data)
+    while len(filtered) > 1:
+        needed_bit = int(compare(sum([b[i] for b in filtered]), len(filtered) / 2))
+        checked = list(filter(lambda x: x[i] == needed_bit, filtered))
+        if len(checked) > 0:
+            filtered = checked
+        if len(filtered) == 1:
+            break
+        i += 1
+    return int("".join(str(x) for x in filtered[0]), 2)
 
-    data = data.splitlines()
-    threshold = len(data) / 2
+
+def xd():
+    data = open(str(Path(__file__).parent.absolute()) + "/input").read().splitlines()
+
+    half_point = len(data) / 2
 
     data = [[int(x) for x in line] for line in data]
-    one_counts = reduce(lambda x, y: [xb + yb for xb, yb in zip(x, y)], data)
-    gamma = sum([int(x > threshold) * (2 ** i) for i, x in enumerate(reversed(one_counts))])
-    epsilon = sum([int(x < threshold) * (2 ** i) for i, x in enumerate(reversed(one_counts))])
-    p1 = gamma * epsilon
+    one_c = reduce(lambda x, y: [xb + yb for xb, yb in zip(x, y)], data)
+    cmn = int("".join(str(int(x >= half_point)) for x in one_c), 2)
 
-    i = 0
-    filtered = list(data)
-    while len(filtered) > 1:
-        needed_bit = int(sum([b[i] for b in filtered]) >= len(filtered) / 2)
-        checked = list(filter(lambda x: x[i] == needed_bit, filtered))
-        if len(checked) > 0:
-            filtered = checked
-        if len(filtered) == 1:
-            break
-        i += 1
-
-    print(filtered[0])
-    oxygen_gen = sum([int(x * (2**i)) for i, x in enumerate(reversed(filtered[0]))])
-    print(oxygen_gen)
-
-    i = 0
-    filtered = list(data)
-    while len(filtered) > 1:
-        needed_bit = int(sum([b[i] for b in filtered]) < len(filtered) / 2)
-        checked = list(filter(lambda x: x[i] == needed_bit, filtered))
-        if len(checked) > 0:
-            filtered = checked
-        if len(filtered) == 1:
-            break
-        i += 1
-
-    co_scrubber = sum([int(x * (2**i)) for i, x in enumerate(reversed(filtered[0]))])
-
-    p2 = co_scrubber * oxygen_gen
+    p1 = cmn * (~cmn & int("1" * len(data[0]), 2))
+    p2 = fltr(data, lambda x, y: x >= y) * fltr(data, lambda x, y: x < y)
 
     print(f"{p1=}")
     print(f"{p2=}")
