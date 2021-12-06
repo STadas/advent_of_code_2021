ab9cddd07cb52639d5627fa341782cbafa2b9e29
TSalten <saltenis.tadas@gmail.com>
2021-12-06 11:33:57 +0200

    day6 clean

--- day06/day06.py
+++ day06/day06.py
@@ -1,28 +1,23 @@
 from pathlib import Path
 
 
-def nemo(counts, days):
-    counts = counts.copy()
-    for day in range(days):
+def xd():
+    data = [int(x) for x in open(str(Path(__file__).parent.resolve()) + "/input").read().split(",")]
+    counts = [data.count(i) for i in range(9)]
+
+    for day in range(256):
+        if day == 80:
+            p1 = sum(counts)
+
         temp = counts[0]
-        for i in range(1, len(counts)):
+        for i in range(1, 9):
             counts[i-1] = counts[i]
         counts[6] += temp
         counts[8] = temp
-    return counts
 
+    p2 = sum(counts)
 
-def xd():
-    data = [int(x) for x in open(str(Path(__file__).parent.resolve()) + "/input").read().split(",")]
-
-    counts = [0] * 9
-    for fish in data:
-        counts[fish] += 1
-
-    p1 = sum(nemo(counts, 80))
     print(f"{p1=}")
-
-    p2 = sum(nemo(counts, 256))
     print(f"{p2=}")
 
 
