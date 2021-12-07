c9079a066bcd233eb82f965b99a07508f670bb4f
TSalten <saltenis.tadas@gmail.com>
2021-12-07 10:55:17 +0200

    day7 clean

--- day07/day07.py
+++ day07/day07.py
@@ -1,24 +1,14 @@
 from pathlib import Path
-from statistics import median, mean
-from math import floor, ceil
 
 
 def xd():
-    data = sorted([int(x) for x in open(str(Path(__file__).parent.resolve()) + "/input").read().split(",")])
+    data = sorted([int(x) for x in open(str(Path(__file__).parent.absolute()) + "/input").read().split(",")])
 
-    sf, sc = 0, 0
-    mf, mc = floor(median(data)), ceil(median(data))
-    for n in data:
-        sf += abs(mf - n)
-        sc += abs(mc - n)
-    p1 = min(sf, sc)
+    m = data[len(data) // 2]
+    p1 = sum(abs(m - n) for n in data)
 
-    sf, sc = 0, 0
-    mf, mc = floor(mean(data)), ceil(mean(data))
-    for n in data:
-        sf += sum(range(abs(mf - n) + 1))
-        sc += sum(range(abs(mc - n) + 1))
-    p2 = min(sf, sc)
+    m = sum(data) // len(data)
+    p2 = sum(((d := abs(m - n)) * (d + 1)) // 2 for n in data)
 
     print(f"{p1=}")
     print(f"{p2=}")
