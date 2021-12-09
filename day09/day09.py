e575e9c08f8f5000dad18fd8c50726737c046fd6
TSalten <saltenis.tadas@gmail.com>
2021-12-09 10:03:47 +0200

    day9

---
+++ day09/day09.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+from functools import reduce
+
+
+def basin(data, x, y, basin_coords):
+    if y > 0 and data[y-1][x] != 9 and data[y-1][x] > data[y][x]:
+        basin_coords.add((x, y-1))
+        basin(data, x, y-1, basin_coords)
+    if y < len(data) - 1 and data[y+1][x] != 9 and data[y+1][x] > data[y][x]:
+        basin_coords.add((x, y+1))
+        basin(data, x, y+1, basin_coords)
+    if x > 0 and data[y][x-1] != 9 and data[y][x-1] > data[y][x]:
+        basin_coords.add((x-1, y))
+        basin(data, x-1, y, basin_coords)
+    if x < len(data[y]) - 1 and data[y][x+1] != 9 and data[y][x+1] > data[y][x]:
+        basin_coords.add((x+1, y))
+        basin(data, x+1, y, basin_coords)
+
+
+def xd():
+    data = open(str(Path(__file__).parent.absolute()) + "/input").read().splitlines()
+    data = [[int(c) for c in line] for line in data]
+
+    p1 = 0
+    basins = []
+    for y, line in enumerate(data):
+        for x, depth in enumerate(line):
+            checkem = []
+            if y > 0:
+                checkem.append(data[y-1][x])
+            if y < len(data) - 1:
+                checkem.append(data[y+1][x])
+            if x > 0:
+                checkem.append(data[y][x-1])
+            if x < len(line) - 1:
+                checkem.append(data[y][x+1])
+            if all(depth < d for d in checkem):
+                p1 += depth + 1
+                basin_coords = set()
+                basin_coords.add((x, y))
+                basin(data, x, y, basin_coords)
+                basins.append(basin_coords)
+
+    print(basins)
+    p2 = reduce(lambda x, y: x * y, sorted(len(b) for b in basins)[-3:])
+
+    print(f"{p1=}")
+    print(f"{p2=}")
+
+
+if __name__ == "__main__":
+    xd()
