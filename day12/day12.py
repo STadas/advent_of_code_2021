26be054ff0654f0f662ede9b5ab39dc298832c6b
TSalten <saltenis.tadas@gmail.com>
2021-12-12 16:36:38 +0200

    day12 cleaner but still slow sad

--- day12/day12.py
+++ day12/day12.py
@@ -1,14 +1,14 @@
 from pathlib import Path
+import time
 
 
 def dfs(graph, p, part2=False):
     if p[-1] == "end":
         return [p]
     done = []
-    k = p[-1]
-    for n in graph[k]:
+    for n in graph[p[-1]]:
         if part2:
-            lowers = tuple(filter(lambda c: c.islower(), p))
+            lowers = tuple(c for c in p if c.islower())
             no_dupes = len(lowers) == len(set(lowers))
         if n.isupper() or ((n not in p or (part2 and no_dupes)) and n != "start"):
             for d in dfs(graph, p + [n], part2):
@@ -19,19 +19,20 @@ def dfs(graph, p, part2=False):
 def xd():
     data = open(str(Path(__file__).parent.absolute()) + "/input").read().splitlines()
 
-    p1 = 0
     graph = {}
     for line in data:
         a, b = line.split("-")
         if a not in graph:
-            graph[a] = []
+            graph[a] = set()
         if b not in graph:
-            graph[b] = []
-        graph[a].append(b)
-        graph[b].append(a)
+            graph[b] = set()
+        graph[a].add(b)
+        graph[b].add(a)
 
+    last = time.time()
     p1 = len(dfs(graph, ["start"]))
     p2 = len(dfs(graph, ["start"], True))
+    print("duration", str(time.time() - last)[:5])
 
     print(f"{p1=}")
     print(f"{p2=}")
