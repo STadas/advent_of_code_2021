8a2add9a9e9f9bbfb16ebed23bec5301aae0e31d
TSalten <saltenis.tadas@gmail.com>
2021-12-14 12:50:04 +0200

    day14 cleaner

--- day14/day14.py
+++ day14/day14.py
@@ -1,25 +1,18 @@
 from pathlib import Path
-
-
-def default_val(d, k, default):
-    if k not in d:
-        d[k] = default
+from collections import Counter
 
 
 def do_steps(p_counts, rules, template, steps):
     for _ in range(steps):
-        n_counts = {}
+        n_counts = Counter()
         for pair, count in p_counts.items():
-            default_val(n_counts, pair1 := pair[0] + rules[pair], 0)
-            default_val(n_counts, pair2 := rules[pair] + pair[1], 0)
-            n_counts[pair1] += count
-            n_counts[pair2] += count
+            n_counts[pair[0] + rules[pair]] += count
+            n_counts[rules[pair] + pair[1]] += count
         p_counts = n_counts
 
-    c_counts = {}
+    c_counts = Counter()
     for pair, count in p_counts.items():
-        default_val(c_counts, ch := pair[0], 0)
-        c_counts[ch] += count
+        c_counts[pair[0]] += count
     c_counts[template[-1]] += 1
 
     return max(c_counts.values()) - min(c_counts.values())
@@ -31,10 +24,9 @@ def xd():
     template = data[0]
     rules = {p: ch for p, ch in (d.split(" -> ") for d in data[2:])}
 
-    p_counts = {}
+    p_counts = Counter()
     for i in range(len(template) - 1):
-        default_val(p_counts, pair := template[i] + template[i+1], 0)
-        p_counts[pair] += 1
+        p_counts[template[i] + template[i+1]] += 1
 
     p1 = do_steps(p_counts.copy(), rules, template, 10)
     p2 = do_steps(p_counts.copy(), rules, template, 40)
