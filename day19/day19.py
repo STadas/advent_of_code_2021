daf57950bfa21161a2ce86d472981d3a02cffc26
TSalten <saltenis.tadas@gmail.com>
2021-12-20 01:06:59 +0200

    day19 cleaner

--- day19/day19.py
+++ day19/day19.py
@@ -33,9 +33,6 @@ class Vector:
     def __hash__(self):
         return hash((self.x, self.y, self.z))
 
-    def __repr__(self):
-        return self.__str__()
-
     def __sub__(self, other):
         return Vector(self.x - other.x,
                       self.y - other.y,
@@ -71,56 +68,50 @@ rotations = {
 
 
 class Scanner:
-    def __init__(self, index, beacons = set([])):
-        self.index = index
+    def __init__(self, beacons):
         self.beacons = beacons
 
     def rotated(self, rot):
-        return Scanner(self.index, set(b.rotated(rot) for b in self.beacons))
+        return Scanner(set(b.rotated(rot) for b in self.beacons))
 
-    def check(self, abs_scanner):
+    def check(self, abs_beacons):
         for rot in rotations:
             rotated_s = self.rotated(rot)
-            distances = rotated_s.distances(abs_scanner.beacons)
-            for distance, count in Counter(distances).items():
-                if count >= 12:
-                    return distance, set(b - distance for b in rotated_s.beacons)
+            dist, count = Counter(rotated_s.distances(abs_beacons)).most_common(1)[0]
+            if count >= 12:
+                return dist, set(b - dist for b in rotated_s.beacons)
 
-    def distances(self, absolute_beacons):
-        return [b2 - b1 for b1, b2 in product(absolute_beacons, self.beacons)]
-
-
-class Probe:
-    def __init__(self):
-        self.unsolved = set()
-
-        self.abs_scanner = None
-        self.scanner_positions = set()
+    def distances(self, abs_beacons):
+        return (b2 - b1 for b1, b2 in product(abs_beacons, self.beacons))
 
 
 def xd():
     data = open(str(Path(__file__).parent.absolute()) + "/input").read().split("\n\n")
-    probe = Probe()
+    unsolved = set()
+    scanner_positions = set()
+    abs_beacons = None
+
     for section in data:
         section = section.splitlines()
-        s = Scanner(int(section[0].split()[2]), set(Vector(*line.split(",")) for line in section[1:]))
-        if probe.abs_scanner is None:
-            probe.abs_scanner = s
+        s = Scanner(set(Vector(*line.split(",")) for line in section[1:]))
+        if abs_beacons is None:
+            abs_beacons = s.beacons
         else:
-            probe.unsolved.add(s)
+            unsolved.add(s)
 
-    while probe.unsolved:
-        for s in probe.unsolved:
-            res = s.check(probe.abs_scanner)
+    while unsolved:
+        print(len(unsolved))
+        for s in unsolved:
+            res = s.check(abs_beacons)
             if res is not None:
-                pos, abs_beacons = res
-                probe.scanner_positions.add(pos)
-                probe.abs_scanner.beacons.update(abs_beacons)
-                probe.unsolved.remove(s)
+                pos, beacons = res
+                scanner_positions.add(pos)
+                abs_beacons.update(beacons)
+                unsolved.remove(s)
                 break
 
-    p1 = len(probe.abs_scanner.beacons)
-    p2 = max(p1.man_dist_to(p2) for p1, p2 in combinations(probe.scanner_positions, 2))
+    p1 = len(abs_beacons)
+    p2 = max(p1.man_dist_to(p2) for p1, p2 in combinations(scanner_positions, 2))
     print(f"{p1=}")
     print(f"{p2=}")
 
