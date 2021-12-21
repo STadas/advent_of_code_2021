5ed1e2986f64e1905505994c6a61c17da9fcd8be
TSalten <saltenis.tadas@gmail.com>
2021-12-21 12:18:58 +0200

    day21 clean

--- day21/day21.py
+++ day21/day21.py
@@ -6,7 +6,6 @@ def roll_det(player_pos, curr_d):
     for _ in range(3):
         player_pos = (player_pos + curr_d - 1) % 10 + 1
         curr_d = curr_d % 100 + 1
-
     return player_pos, curr_d
 
 
@@ -14,7 +13,7 @@ def roll_dirac(pos_1, pos_2, s1=0, s2=0, cache={}):
     if (pos_1, pos_2, s1, s2) in cache:
         return cache[pos_1, pos_2, s1, s2]
 
-    w1, w2 = 0, 0
+    w1 = w2 = 0
     for rolls in product((1, 2, 3), repeat=3):
         n_pos = (pos_1 + sum(rolls) - 1) % 10 + 1
         if s1 + n_pos >= 21:
@@ -32,18 +31,16 @@ def xd():
     data = open(str(Path(__file__).parent.absolute()) + "/input").read().splitlines()
     pos_1, pos_2 = (int(section.split()[-1]) for section in data)
     
-    s1, s2 = 0, 0
+    roll_count = s1 = s2 = 0
     curr_d = 1
-    roll_count = 0
     while s1 < 1000 and s2 < 1000:
         pos_1, curr_d = roll_det(pos_1, curr_d)
-        s1 += pos_1
-        s1, s2 = s2, s1
+        s1, s2 = s2, s1 + pos_1
         pos_1, pos_2 = pos_2, pos_1
         roll_count += 3
 
     p1 = min(s1, s2) * roll_count
-    p2 = max(roll_dirac(*(int(section.split()[-1]) for section in data), 0, 0))
+    p2 = max(roll_dirac(*(int(section.split()[-1]) for section in data)))
 
     print(f"{p1=}")
     print(f"{p2=}")
